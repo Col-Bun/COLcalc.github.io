219144b304cf23248f0a28a3674f7176b650eb5f
Col-Bun <brudontbuccelarti@gmail.com>
2026-03-29 09:54:42 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,20 @@
-var CACHE_NAME = 'cop-usd-v3';
+var CACHE_NAME = 'cop-usd-v4';
 var ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function(e) {
-    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(ASSETS); }));
+    e.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(ASSETS);
+    }));
     self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
     e.waitUntil(caches.keys().then(function(keys) {
-        return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+        return Promise.all(keys.filter(function(k) {
+            return k !== CACHE_NAME;
+        }).map(function(k) {
+            return caches.delete(k);
+        }));
     }));
     self.clients.claim();
 });
@@ -18,10 +24,16 @@ self.addEventListener('fetch', function(e) {
     if (url.indexOf('exchangerate') !== -1) {
         e.respondWith(fetch(e.request).then(function(response) {
             var clone = response.clone();
-            caches.open(CACHE_NAME).then(function(cache) { cache.put(e.request, clone); });
+            caches.open(CACHE_NAME).then(function(cache) {
+                cache.put(e.request, clone);
+            });
             return response;
-        }).catch(function() { return caches.match(e.request); }));
+        }).catch(function() {
+            return caches.match(e.request);
+        }));
         return;
     }
-    e.respondWith(caches.match(e.request).then(function(cached) { return cached || fetch(e.request); }));
+    e.respondWith(caches.match(e.request).then(function(cached) {
+        return cached || fetch(e.request);
+    }));
 });
